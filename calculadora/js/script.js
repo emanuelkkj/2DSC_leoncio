6b9afc5bf81df8dc0fbe8a1d835775e466cafb1b
emanuelkkj <emanuelfdelima19@gmail.com>
2024-10-03 15:21:58 +0000

    Iniciado o código JavaScript

---
+++ calculadora/js/script.js
@@ -0,0 +1,9 @@
+// FAZ REFERÊNCIA AOS ELEMENTOS DOM
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btcalcular = document.querySelector('#btn-calcular')
+alert(numero1)
+// CRIA A FUNÇÃO CALCULAR
+
+
+// ADICIONA ESCUTAR DE EVENTOS
